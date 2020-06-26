4b8b0f5b3de6223e10eafdcb64ebe79b02296635
Gache <erickfrancodelgado@hotmail.com>
2020-06-26 17:00:08 +0200

    Exercice validation formulaire

---
+++ html_Css_Js/vendredi_26_juin/exercicesPassword/exercicesValidation.js
@@ -0,0 +1,23 @@
+function isSamePassword(formulaire) {
+  //  recuperation de la valeur de mes input
+  let pwd = formulaire.pwd.value;
+  let pwdVerif = formulaire.pwd_verif.value;
+  let btn = document.getElementById("btn_submit");
+
+  // Condition pour verifier si le mot de passe ont un minimun 6 caracter
+  if (pwdVerif.length > 5) {
+    /* condition pour savoir si le mot de passe  sont identique */
+    if (pwd === pwdVerif) {
+      console.log("ok");
+      btn.disabled = false;
+      formulaire.pwd_verif.style.border = "2px solid green";
+    } else {
+      console.log("les mot de passe ne correspond pas");
+      btn.disabled = true;
+      formulaire.pwd_verif.style.border = "2px solid red";
+    }
+  } else {
+    btn.disabled = true;
+    formulaire.pwd_verif.style.border = "2px solid red";
+  }
+}
